935ce93dfb0086c91037eb85b70b1a431c25bb82
Arsenymlwww <sentyaabrr@gmail.com>
2026-06-04 20:11:44 +0300

    complete task 08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const CopyState = [...state]
+            if (action.payload === 'up'){
+                CopyState.sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if (action.payload === 'down'){
+                CopyState.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return CopyState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter( u => u.age >= action.payload)
+            // return state // need to fix
         }
         default:
             return state
